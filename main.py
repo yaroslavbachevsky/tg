97af5910beac8aaebced3c8c43e795d7ed2a9fb2
yaroslavbachevsky <52602334+yaroslavbachevsky@users.noreply.github.com>
2019-07-06 15:56:08 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from telegram.client import Telegram
 import requests
 
-def log(msg):
+def pushlog(msg):
     requests.post("http://pushmebot.ru/send", data={'key': '1a8a8768c2d7c7a674f3355c4a917dd8', 'message': msg})
+
+pushlog("BUILDING")
 
 tg = Telegram(
     api_id='846196',
     api_hash='8a83b26ce76d21f97851ac70329a9158',
     phone='+79649824012',
-    database_encryption_key='randomkey'
-)
+    database_encryption_key='randomkey')
 
 tg.login()
 
@@ -21,10 +22,9 @@ def new_message_handler(update):
 
     if message_text == 'ping':
         chat_id = update['message']['chat_id']
-        log(f'Ping has been received from {chat_id}')
-        tg.send_message(
-            chat_id=chat_id,
-            text='pong')
+        pushlog(f'Ping has been received from {chat_id}')
+        tg.send_message(chat_id=chat_id, text='pong')
 
 tg.add_message_handler(new_message_handler)
+pushlog("build OK!")
 tg.idle()
