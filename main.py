b23ef2be7e0b946646a549f2ce14d71c7ea56416
yaroslavbachevsky <52602334+yaroslavbachevsky@users.noreply.github.com>
2019-07-06 16:25:23 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-from telegram.client import Telegram
 import requests
+import json
+from telegram.client import Telegram
 
-def pushlog(msg):
-    requests.post("http://pushmebot.ru/send", data={'key': '1a8a8768c2d7c7a674f3355c4a917dd8', 'message': msg})
+# loading config
+with open('config.json', 'r', encoding='utf8') as f:
+	conf = json.load(f)
 
-pushlog("BUILDING")
+# log to PushMe bot
+def pushme(msg):
+    requests.post("http://pushmebot.ru/send", data={'key': conf['pushmekey'], 'message': msg})
 
-tg = Telegram(
-    api_id='846196',
-    api_hash='8a83b26ce76d21f97851ac70329a9158',
-    phone='+79649824012',
-    database_encryption_key='randomkey')
+pushme("Restarting worker...")
 
-tg.login()
+try:
+    tg = Telegram(
+        api_id=conf['auth']['id'],
+        api_hash=conf['auth']['hash'],
+        phone=conf['auth']['phone'],
+        database_encryption_key=conf['auth']['database_key'])
 
-log('login SUCCESS!')
+    tg.login()
+except:
+    pushme('Login ERROR(')
+else:
+    pushme('Login success!')
 
 def new_message_handler(update):
     message_content = update['message']['content'].get('text', {})
     message_text = message_content.get('text', '').lower()
-
     if message_text == 'ping':
         chat_id = update['message']['chat_id']
-        pushlog(f'Ping has been received from {chat_id}')
+        pushme(f'Received new message from {chat_id}')
         tg.send_message(chat_id=chat_id, text='pong')
 
+
 tg.add_message_handler(new_message_handler)
-pushlog("build OK!")
+pushme("Build OK!")
 tg.idle()
